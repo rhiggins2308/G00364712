c63e28ae5a6e1bc4c4e8282bbd010ba628d8c3ec
rhiggins2308 <35814315+rhiggins2308@users.noreply.github.com>
2018-02-23 19:42:29 +0000

    Add files via upload

--- projectEulerProb5.py
+++ projectEulerProb5.py
@@ -2,13 +2,22 @@
 # 12th Feb 2018
 # Euler Project Problem 5: Smallest multiple of numbers from 1 - 20
 
+def lcm(x, y):
+    if x > y:
+        greater = x
+    else:
+        greater = y
+    while (True):
+        if ((greater % x == 0) and (greater % y == 0)):
+            lcm = greater
+            break
+        greater += 1
+    return lcm
+
 prod = 1
-num = 1
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 
-while num <= 20:
-    if prod % num != 0:
-        prod = num*prod
-    num +=1
+for n in range(len(nums)):
+    prod = lcm(prod, nums[n])
 
-print("LCM of all numbers from 1 - 20, inclusive, is:", prod)
-    
+print("LCM is: ", prod)
